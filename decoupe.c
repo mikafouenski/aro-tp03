44356b7a1b36e2401feb497a5076b573c7b47b50
BERNARDINI Mickael <b13002175@tempo21-2.luminy.univ-amu.fr>
2017-02-09 18:27:37 +0100

    inter

--- decoupe.c
+++ decoupe.c
@@ -2,121 +2,139 @@
 #include <stdlib.h>           /* C standard library                   */
 #include <glpk.h>             /* GNU GLPK linear/mixed integer solver */
 
-int main(){
-	int i,j,k = 1;
-	double matrix_coef[20] = {2.0,1.0,1.0,1.0,1.0, 0.0,1.0,1.0,0.0,0.0, 0.0,0.0,0.0,1.0,1.0, 0.0,1.0,0.0,1.0,0.0};
-
+void sacADos(int y1, int y2, int y3, int y4,double* resultat){
+    int i,j,k;
+    double matrix_coef[4] = {45,36,31,14};
+    double matrix_resultat[4] = {y1,y2,y3,y4};
+    char name[10] ;
     glp_prob *lp;
+    int ia[1+1000], ja[1+1000];
+    double ar[1+1000];
 
-	int ia[1+1000], ja[1+1000];
+    lp = glp_create_prob();
+    glp_set_prob_name(lp, "Sac a dos");
+    glp_set_obj_dir(lp, GLP_MAX);
+    glp_add_rows(lp, 1);
+    glp_set_row_name(lp, 1, "SOMME");
+    glp_set_row_bnds(lp, 1, GLP_UP, 0, 100);
+    
+    glp_add_cols(lp, 4);
+    for(i=0;i<4;i++){
+        snprintf(name,10, "P%d", i+1);
+        glp_set_col_name(lp, i+1, name);
+        glp_set_col_bnds(lp, i+1, GLP_LO, 0.0, 0.0);
+        glp_set_obj_coef(lp, i+1, matrix_resultat[i]);
+        glp_set_col_kind(lp,i+1,GLP_CV);
+    }
+    
+    k=1;
+    for(j=1;j<=4;j++){
+            ia[k] = 1; ja[k] = j; ar[k] = matrix_coef[k-1];
+            k++;
+    }
+    glp_load_matrix(lp, 4, ia, ja, ar);
+    glp_simplex(lp, NULL);
+    resultat[0] =  glp_get_obj_val(lp);  
+    for(i =i ;i<5;i++){
+        resultat[i]=glp_get_col_prim(lp, i-1);
+    }                 
+    glp_delete_prob(lp);
+}
 
-	double ar[1+1000], z, x1, x2, x3, x4, x5, y1, y2, y3, y4;
+void newMatCoef( double* matrix_coef, double* matrice_sac, int taille){
+    int i;
+    double newMat[taille+4];
+    for(i=0;i<taille;i++){
+        newMat[i]=matrix_coef[i];
+    }
+    for(i=0;i<4;i++){
+        newMat[taille+i]=matrice_sac[i];
+    }
+    matrix_coef=newMat;
+}
 
-	lp = glp_create_prob();
-	glp_set_prob_name(lp, "découpe");
-	glp_set_obj_dir(lp, GLP_MIN);
-	glp_add_rows(lp, 4);
-	glp_set_row_name(lp, 1, "R1");
-	glp_set_row_bnds(lp, 1, GLP_LO, 97.0, 0.0);
-	glp_set_row_name(lp, 2, "R2");
-	glp_set_row_bnds(lp, 2, GLP_LO, 610.0, 0.0);
-	glp_set_row_name(lp, 3, "R3");
-	glp_set_row_bnds(lp, 3, GLP_LO, 395.0, 0.0);
-	glp_set_row_name(lp, 4, "R4");
-	glp_set_row_bnds(lp, 4, GLP_LO, 211.0, 0.0);
+double* resolutionGLPK(double* matrix_coef,double* matrix_commande, int nbContrainte, int nbVariable, char* nom, int MIN_MAX){
+    int i,j,k = 1;
+    int tailleMat = 20;
+    double y1, y2, y3, y4;
+    double ResultatSacADos[5];
+    glp_prob *lp;
+    char name[10];
+    double ar[1+1000];
+    int ia[1+1000], ja[1+1000];
+   
+    for(;;){
+        lp = glp_create_prob();
+        glp_set_obj_dir(lp, MIN_MAX);
+        glp_set_prob_name(lp, nom);
+        glp_add_rows(lp, nbContrainte);
 
-	glp_add_cols(lp, 5);
-	glp_set_col_name(lp, 1, "P1");
-	glp_set_col_bnds(lp, 1, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp, 1, 1.0);
-	glp_set_col_name(lp, 2, "P2");
-	glp_set_col_bnds(lp, 2, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp, 2, 1.0);
-	glp_set_col_name(lp, 3, "P3");
-	glp_set_col_bnds(lp, 3, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp, 3, 1.0);
-	glp_set_col_name(lp, 4, "P4");
-	glp_set_col_bnds(lp, 4, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp, 4, 1.0);
-	glp_set_col_name(lp, 5, "P5");
-	glp_set_col_bnds(lp, 5, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp, 5, 1.0);
+        for(i=0;i<nbContrainte;i++){
+            snprintf(name,10, "R%d", i+1);
+            glp_set_row_name(lp, i+1,name);
+            glp_set_row_bnds(lp, i+1, GLP_LO, matrix_commande[i], 0.0);
+        }
+        glp_add_cols(lp, nbVariable);
+        
+        for(i=0;i<nbVariable;i++){
+            snprintf(name,10, "P%d", i+1);
+            glp_set_col_name(lp, i+1, name);
+            glp_set_col_bnds(lp, i+1, GLP_LO, 0.0, 0.0);
+            glp_set_obj_coef(lp, i+1, 1.0);
+        }
 
-	for(i=1;i<5;i++){
-		for(j=1;j<6;j++){
-			ia[k] = i; ja[k] = j; ar[k] = matrix_coef[k-1];
-			k++;
-		}
-	}
+        for(j=1;j<=nbVariable;j++){
+            for(i=1;i<=nbContrainte;i++){
+                ia[k] = i; ja[k] = j; ar[k] = matrix_coef[k-1];
+                k++;
+            }
+        }
 
-	glp_smcp parm;
-	glp_init_smcp(&parm);
-	parm.meth = GLP_DUAL;
+        glp_load_matrix(lp, nbContrainte*nbVariable, ia, ja, ar);
+        glp_simplex(lp, NULL);
+        glp_write_lp(lp,NULL,"Prog.txt");
+        
 
-	glp_load_matrix(lp, 20, ia, ja, ar);
-	glp_simplex(lp, NULL);
-	glp_simplex(lp, &parm);
-	z = glp_get_obj_val(lp);
-	x1 = glp_get_col_prim(lp, 1);
-	x2 = glp_get_col_prim(lp, 2);
-	x3 = glp_get_col_prim(lp, 3);
-	x4 = glp_get_col_prim(lp, 4);
-	x5 = glp_get_col_prim(lp, 5);
+        glp_smcp parm;
+        glp_init_smcp(&parm);
+        parm.meth = GLP_DUAL;
+        glp_simplex(lp, &parm);
 
-	y1 = glp_get_row_dual(lp,1);
-	y2 = glp_get_row_dual(lp,2);
-	y3 = glp_get_row_dual(lp,3);
-	y4 = glp_get_row_dual(lp,4);
+        y1 = glp_get_row_dual(lp,1);
+        y2 = glp_get_row_dual(lp,2);
+        y3 = glp_get_row_dual(lp,3);
+        y4 = glp_get_row_dual(lp,4);
+        sacADos(y1,y2,y3,y4,ResultatSacADos);
+        glp_delete_prob(lp);
+        //if (ResultatSacADos[0]==0){break;}
 
-	/*SAC A DOS*/
+        //newMatCoef(matrix_coef,ResultatSacADos,tailleMat);
+        tailleMat += 4;
+    }
 
-	double matrix_coef2[4] = {45,36,31,14};
+}
 
-	glp_prob *lp2;
-	int ia2[1+1000], ja2[1+1000];
-	double ar2[1+1000];
+void resolution(double* matrix_coef,double* matrix_commande, int nbContrainte, int nbVariable){
+    
+   //double z, x1, x2, x3, x4, x5, y1, y2, y3, y4, resultatSacADos;
+    resolutionGLPK(matrix_coef,matrix_commande,nbContrainte,nbVariable,"Decoupe",GLP_MIN);
 
-	lp2 = glp_create_prob();
-	glp_set_prob_name(lp2, "Sac a dos");
-	glp_set_obj_dir(lp2, GLP_MAX);
-	glp_add_rows(lp2, 1);
-	glp_set_row_name(lp2, 1, "SOMME");
-	glp_set_row_bnds(lp2, 1, GLP_UP, 0, 100);
 
-	glp_add_cols(lp2, 4);
-	glp_set_col_name(lp2, 1, "P1");
-	glp_set_col_bnds(lp2, 1, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp2, 1, y1);
-	glp_set_col_kind(lp2,1,GLP_CV);
+    //z = glp_get_obj_val(lp);
+    /*x1 = glp_get_col_prim(lp, 1);
+    x2 = glp_get_col_prim(lp, 2);
+    x3 = glp_get_col_prim(lp, 3);
+    x4 = glp_get_col_prim(lp, 4);
+    x5 = glp_get_col_prim(lp, 5);
+    */
 
-	glp_set_col_name(lp2, 2, "P2");
-	glp_set_col_bnds(lp2, 2, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp2, 2, y2);
-	glp_set_col_kind(lp2,2,GLP_CV);
+    //printf("\nz = %g; x1 = %g; x2 = %g; x3 = %g; x4= %g; x5= %g; y1 = %g, y2 = %g, y3 = %g, y4 = %g, resultatSacADos = %g \n",z, x1, x2, x3, x4, x5,y1,y2,y3,y4,resultatSacADos);
 
-	glp_set_col_name(lp, 3, "P3");
-	glp_set_col_bnds(lp2, 3, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp2, 3, y3);
-	glp_set_col_kind(lp2,3,GLP_CV);
+}
+ 
 
-	glp_set_col_name(lp2, 4, "P4");
-	glp_set_col_bnds(lp2, 4, GLP_LO, 0.0, 0.0);
-	glp_set_obj_coef(lp2, 4, y4);
-	glp_set_col_kind(lp2,4,GLP_CV);
-
-	k=1;
-	for(j=1;j<5;j++){
-			ia2[k] = 1; ja2[k] = j; ar2[k] = matrix_coef2[k-1];
-			k++;
-	}
-	glp_load_matrix(lp2, 4, ia2, ja2, ar2);
-	glp_simplex(lp2, NULL);
-
-	/*FIN SAC A DOS */
-
-	glp_write_lp(lp,NULL,"Prog.txt");
-	glp_write_lp(lp2,NULL,"SacADos.txt");
-	printf("\nz = %g; x1 = %g; x2 = %g; x3 = %g; x4= %g; x5= %g; y1 = %g, y2 = %g, y3 = %g, y4 = %g \n",z, x1, x2, x3, x4, x5,y1,y2,y3,y4);
-	glp_delete_prob(lp);
-	glp_delete_prob(lp2);
+int main(){
+    double matrix_coef[20] = {2,0,0,0,1,1,0,1,1,1,0,0,1,0,1,1,1,0,1,0}; //les cinqs premieres colonnes
+    double matrix_commande[4] = {97.0,610.0,395.0,211.0};
+    resolution(matrix_coef,matrix_commande,4,5);
 }
--- decoupe_opti.c
+++ decoupe_opti.c
@@ -4,57 +4,94 @@
 #define BUFFSIZE 1000
 #define ZERO 0.0
 #define COEFS 1000
+#define NBCONSTRAINTS 4
 
 typedef struct matrix {
     int constraints[COEFS + 1];
     int variables[COEFS + 1];
     double coefs[COEFS + 1];
-    int size;
+    int next;
 } matrix;
 
 matrix* newMatrix() {
     matrix* m = (matrix*) malloc(sizeof(matrix));
-    m->size = 1;
+    m->next = 1;
     return m;
 }
 
 void addCoef(matrix* m, int constraint, int variable, double coef) {
-    m->constraints[m->size] = constraint;
-    m->variables[m->size] = variable;
-    m->coefs[m->size] = coef;
-    m->size = m->size + 1;
+    m->constraints[m->next] = constraint;
+    m->variables[m->next] = variable;
+    m->coefs[m->next] = coef;
+    m->next = m->next + 1;
+}
+
+void initMatrix(matrix* m) {
+    double init[5][4] = {{2, 0, 0, 0},
+                         {1, 1, 0, 1},
+                         {1, 1, 0, 0},
+                         {1, 0, 1, 1},
+                         {1, 0, 1, 0}};
+
+    for (int j = 1; j <= 5; ++j)
+        for (int i = 1; i <= NBCONSTRAINTS; ++i)
+            addCoef(m, i, j, init[j][i]);
 }
 
 void loadMatrix(glp_prob* lp, matrix* m) {
-    glp_load_matrix(lp, m->size - 1, m->constraints, m->variables, m->coefs);
+    glp_load_matrix(lp, m->next - 1, m->constraints, m->variables, m->coefs);
 }
 
-void addAuxVariables(glp_prob* lp, double* coefs, int start, int end, int bound) {
-    glp_add_rows(lp, (end - start));
+void addConstraints(glp_prob* lp, double* coefs, int start, int end, int bound) {
+    glp_add_rows(lp, end);
     for (int i = start; i <= end; ++i) {
         char name[BUFFSIZE];
         sprintf(name, "aux_%d", i);
         glp_set_row_name(lp, i, name);
-        glp_set_row_bnds(lp, i, bound, coefs[i], ZERO);
+        glp_set_row_bnds(lp, i, bound, coefs[i - 1], ZERO);
     }
 }
 
-void addPositivityConstraints(glp_prob* lp, int start, int end) {
-    glp_add_cols(lp, (end - start));
+void addVarrables(glp_prob* lp, int start, int end) {
+    glp_add_cols(lp, end);
     for (int i = start; i <= end; ++i) {
         char name[BUFFSIZE];
-        sprintf(name, "positivity_%d", i);
+        sprintf(name, "x_%d", i);
         glp_set_col_name(lp, i, name);
         glp_set_col_bnds(lp, i, GLP_LO, ZERO, ZERO);
         glp_set_obj_coef(lp, i, 1.0);
     }
 }
 
-void generateColumn(int i, int j) {
-    /* TODO */
-}
-
 int main(int argc, char const *argv[]) {
+    double coefs[NBCONSTRAINTS] = {97, 610, 395, 211};
+
+    glp_prob *lp = glp_create_prob();
+    matrix* m = (matrix*) malloc (sizeof(matrix));
+    glp_set_prob_name(lp, "decoupe");
+    glp_set_obj_dir(lp, GLP_MIN);
+
+    addConstraints(lp, coefs, 1, NBCONSTRAINTS, GLP_LO);
+    addVarrables(lp, 1, 2);
+    printf("Ajout OK.\n");
+
+    glp_smcp parm;
+    glp_init_smcp(&parm);
+    parm.meth = GLP_DUAL;
+
+    initMatrix(m);
+
+    loadMatrix(lp, m);
+
+    glp_simplex(lp, &parm);
+
+    glp_write_lp(lp, NULL ,"Prog.txt");
+
+    // double result[4];
+    // for (int i = 0; i < 4; ++i){
+    //     result[i] = glp_get_row_dual(lp,i);
+    // } 
+
 
     return 0;
 }
